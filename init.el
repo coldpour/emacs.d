edaabab21d3b54b721e9f6ac08508d95c0a16f90
coldpour <coldpour@gmail.com>
2026-02-21 20:44:30 -0800

    reset, clean up

--- init.el
+++ init.el
@@ -1,13 +1,11 @@
 
 (toggle-frame-maximized)
-(toggle-frame-fullscreen)
+;(toggle-frame-fullscreen)
 (setq inhibit-startup-message t)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (tooltip-mode -1)
 (set-fringe-mode 0)
-
-(load-theme 'tango-dark)
 
 (eval-and-compile
   (customize-set-variable
@@ -21,6 +19,11 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+;; Keep machine-local Custom settings and secrets out of git.
+(setq custom-file (expand-file-name "local/custom.el" user-emacs-directory))
+(make-directory (file-name-directory custom-file) t)
+(load custom-file 'noerror 'nomessage)
+
 (column-number-mode)
 (global-display-line-numbers-mode t)
 
@@ -29,51 +32,8 @@
 		eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
-(use-package command-log-mode)
-(clm/open-command-log-buffer)
-(global-command-log-mode)
-
-(find-file "init.el")
+(find-file (expand-file-name "init.el" user-emacs-directory))
 (switch-to-buffer-other-window "*Messages*")
-
-(use-package ivy
-  :diminish
-  :bind (;("C-s" . swiper)
-	 :map ivy-minibuffer-map
-	 ("TAB" . ivy-alt-done)
-	 ("C-l" . ivy-alt-done)
-	 ("C-j" . ivy-next-line)
-	 ("C-k" . ivy-previous-line)
-
-	 :map ivy-switch-buffer-map
-	 ("C-k" . ivy-previous-line)
-	 ("C-l" . ivy-done)
-	 ("C-d" . ivy-switch-buffer-kill)
-	 :map ivy-reverse-i-search-map
-	 ("C-k" . ivy-previous-line)
-	 ("C-d" . ivy-reverse-i-search-kill))
-  :init (ivy-mode 1))
-
-;; NOTE: The first time you load your configuration on a new machine, you'll
-;; need to run the following command interactively so that mode line icons
-;; display correctly:
-;;
-;; M-x all-the-icons-install-fonts
-
-(use-package all-the-icons)
-
-(use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode 1)
-  :custom ((doom-modeline-height 15)))
-
-(use-package doom-themes)
-; counsel-load-theme
-; dracula
-					; pale-night
-
-(use-package rainbow-delimiters	
-  :hook (prog-mode . rainbow-delimiters-mode))
 
 (use-package which-key
   :init (which-key-mode)
@@ -81,24 +41,8 @@
   :config
   (setq which-key-idle-delay 0.1))
 
-(use-package counsel
-  :bind (("M-x" . counsel-M-x)
-	 ("C-x b" . counsel-ibuffer)
-	 ("C-x C-f" . counsel-find-file)
-	 :map minibuffer-local-map
-	 ("C-r" . 'counsel-minibuffer-history)))
-
-(use-package ivy-rich
-  :init
-  (ivy-rich-mode 1))
-
 (use-package helpful
-  :ensure t
-  :custom
-  (counsel-describe-function-function #'helpful-callable)
-  (counsel-describe-variable-function #'helpful-variable)
-  :bind
-  ([remap describe-function] . counsel-describe-function)
-  ([remap describe-command] . helpful-command)
-  ([remap describe-variable] . counsel-describe-variable)
-  ([remap describe-key] . helpful-key))
+  :ensure t)
+
+(use-package magit
+  :bind (("C-x g" . magit-status)))
